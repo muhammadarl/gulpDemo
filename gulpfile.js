432a337dbf0584b620f2508d71195416f69e70b2
muhammadarl <syiarul45@gmail.com>
2025-01-19 00:09:06 +0700

    feat: adding yaml and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+import { task, src, dest, series } from 'gulp';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
+import cleanCSS from 'gulp-clean-css';
+
+// Task to minify and concatenate JS files
+task('scripts', function () {
+  return src('resources/js/**/*.js') // Source JS files
+    .pipe(concat('app.js'))               // Concatenate into app.js
+    .pipe(uglify())                       // Minify JS
+    .pipe(dest('public/js'));        // Output to public/js
+});
+
+// Task to minify CSS files
+task('styles', function () {
+  return src('resources/css/**/*.css') // Source CSS files
+    .pipe(concat('app.css'))                 // Concatenate into app.css
+    .pipe(cleanCSS())                        // Minify CSS
+    .pipe(dest('public/css'));         // Output to public/css
+});
+
+// Default task that runs both the JS and CSS tasks
+task('default', series('scripts', 'styles'));
